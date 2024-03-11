bc2607aecfec467b77940e933351bfe43c2f207a
MohamedBellal <162454805+MohamedBellal@users.noreply.github.com>
2024-03-11 01:07:08 +0100

    test retour

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import javax.swing.*;
 
 public class Main extends JFrame {
     public Main() {
+        super("isaac mais en mieux");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 200);
+        setLocationRelativeTo(null); // Center the window
+        JPanel panel = new JPanel();
+        add(panel);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
